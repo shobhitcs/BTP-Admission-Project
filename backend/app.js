f148f8a778f82ad46c0451c06af102c85dea9e94
shobhitcs <shobhitchauhancs@gmail.com>
2025-05-06 22:51:08 +0530

    Login fix

--- backend/app.js
+++ backend/app.js
@@ -25,19 +25,19 @@ require("dotenv").config();
 // app.use(morgan('dev'))
 // app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: [process.env.NODE_ENV !== "production" && process.env.BACKEND_URL],
-//     credentials: true,
-//   })
-// );
-
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [process.env.NODE_ENV !== "production" && process.env.BACKEND_URL],
     credentials: true,
   })
 );
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(cookieParser());
 // app.use((req, res, next) => {
